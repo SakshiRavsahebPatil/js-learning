72484380720e4741715defb6dd7b91832751b6a0
SakshiRavsahebPatil <160692592+SakshiRavsahebPatil@users.noreply.github.com>
2024-03-08 18:14:18 +0530

    01-variableAssign
    01-variableAssign

---
+++ 01-variableAssign.js
@@ -0,0 +1,45 @@
+console.log("==========step 1==========");
+console.log("...before init...");
+var myFullName;
+console.log("My Full Name before Initialization is :" , myFullName);
+myFullName = "sakshi Patil"
+
+console.log("...after init...");
+console.log("My Full Name after Initialization is :" , myFullName);
+
+myFullName = "sakshi Ravsaheb Patil"
+console.log("My Full Name after Initialization is :" , myFullName);
+
+console.log("==========step 2==========");
+var myLove;
+myLove = "I love only JS"
+console.log("My love is:" , myLove);
+
+console.log("==========step 3==========");
+var firstName;
+firstName = "Sakshi"
+console.log("First Name :" ,  firstName);
+
+var lastName;
+lastName = "Patil"
+console.log("Last Name :" ,  lastName);
+
+var age;
+age = 21 
+console.log("Age:",age);
+
+var add ;
+add ="Atpadi"
+console.log("Address :" , add);
+
+var pinCode ;
+pinCode = 415301 
+console.log("Pin Code :" , pinCode );
+
+var gender ;
+gender = "Female"
+console.log("Gender :" , gender);
+
+var isMarried ;
+isMarried = "false"
+console.log("Is Married :" , isMarried);
