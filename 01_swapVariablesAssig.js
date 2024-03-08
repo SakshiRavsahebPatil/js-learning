7432731f9022057dc94a0db6140834f22e72b8e8
SakshiRavsahebPatil <160692592+SakshiRavsahebPatil@users.noreply.github.com>
2024-03-08 18:56:42 +0530

    01_swapVariablesAssignment
    01_swapVariablesAssignment

---
+++ 01_swapVariablesAssig.js
@@ -0,0 +1,38 @@
+console.log("=========step 1=========");
+console.log("...==swap two variable values using third variable==...");
+var sweety ;
+sweety = "Sweety" 
+var cutie = "Cutie" ;
+console.log("======== Before Swap ==========");
+console.log("sweety :" , sweety );
+console.log("Cutie :" , cutie );
+
+console.log("======== After Swap ==========");
+var thirdValue = sweety ;
+
+sweety = cutie ;
+cutie = thirdValue;
+console.log("sweety :" , sweety );
+console.log("Cutie :" , cutie );
+
+console.log("=========step 2=========");
+console.log("...==swap third variable values==...");
+var n1 = "A" ;
+var n2 = "B" ;
+var n3 = "C" ;
+console.log("======Before Swap========");
+console.log("n1 :" , n1);
+console.log("n2 :" , n2);
+console.log("n3 :" , n3);
+
+console.log("======After Swap========");
+var temp = n1;
+
+n1 = n2 ;
+n2 = n3 ;
+n3 = temp ;
+console.log("n1 :" , n1);
+console.log("n2 :" , n2);
+console.log("n3 :" , n3);
+
+
