1f39b39442007c5e51d2c63e448edaba6a41630f
SakshiRavsahebPatil <160692592+SakshiRavsahebPatil@users.noreply.github.com>
2024-03-09 11:58:53 +0530

    02_typeOfassignment
    02_typeOfassignment

---
+++ 02_typeOf.js
@@ -0,0 +1,41 @@
+console.log("====================step 1====================");
+var bankName ;
+console.log("=====before initialization=====");
+console.log("value of bankName :" , bankName);
+console.log("type of bankName :" , bankName);
+
+console.log("=====After initialization=====");
+bankName = "SBI Bank" ;
+console.log("value of bankName :" , bankName);
+var typeOfBankName = typeof bankName ;
+console.log("type of bankName :" ,  typeOfBankName);
+
+console.log("====================step 2====================");
+var mark = "90%" ;
+console.log("value of mark :" , mark);
+var typeOfMark = typeof mark ;
+console.log("type of mark :" ,  typeOfMark);
+
+console.log("====================step 3====================");
+var isWorking ;
+console.log("=====before initialization=====");
+console.log("value of IsWorking :" , isWorking);
+console.log("type of IsWorking :" , isWorking);
+
+console.log("=====After initialization=====");
+isWorking = true ;
+console.log("value of IsWorking :" ,isWorking);
+var typeOfIsWorking = typeof  isWorking;
+console.log("type of IsWorking :" , typeOfIsWorking );
+
+console.log("====================step 4====================");
+var totalCount = "Hundred and Seven" ;
+console.log("vale of totalCount :" , totalCount);
+var typeOfTotalCount = typeof  totalCount;
+console.log("Type of totalCount :" ,typeOfTotalCount );
+
+console.log("=====update Variable=====");
+var totalCount = "107" ;
+console.log("vale of totalCount :" , totalCount);
+var typeOfTotalCount = typeof  totalCount;
+console.log("Type of totalCount :" ,typeOfTotalCount );
