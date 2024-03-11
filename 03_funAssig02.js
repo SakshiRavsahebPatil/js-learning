6593ab17a166f2af1244d4941d15a672420140e9
SakshiRavsahebPatil <160692592+SakshiRavsahebPatil@users.noreply.github.com>
2024-03-11 22:00:36 +0530

    03_funAssig02_session24
    03_funAssig02_session24

---
+++ 03_funAssig02.js
@@ -0,0 +1,27 @@
+function bandDetails(bankName, accountNum, location, pinCode){
+    console.log("Bnak Name is :",bankName);
+    console.log("Account Number is :",accountNum);
+    console.log("Location is :",location);
+    console.log("Pin Code :",pinCode);
+}
+bandDetails("CITI Bank", 3456782345, 'Pune', 431202);
+console.log("----------------------------------------------------------------------------------------------");
+
+
+function bandDetails(bankName, accountNum, location, pinCode){
+    console.log("Bnak Name is :",bankName);
+    console.log("Account Number is :",accountNum);
+    console.log("Location is :",location);
+    console.log("Pin Code :",pinCode);
+}
+bandDetails("Axis Bank", 7856782345, "Mumbai", 441202);
+console.log("----------------------------------------------------------------------------------------------");
+
+
+function bandDetails(bankName, accountNum, location, pinCode){
+    console.log("Bnak Name is :",bankName);
+    console.log("Account Number is :",accountNum);
+    console.log("Location is :",location);
+    console.log("Pin Code :",pinCode);
+}
+bandDetails("HDFC Bank", 8956782345, "Pune", 631202, "opne");
