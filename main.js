512abfb91708fef335b501424c9f54129245ad07
SakshiRavsahebPatil <160692592+SakshiRavsahebPatil@users.noreply.github.com>
2024-04-13 10:30:54 +0530

    24_sortingAssignB
    24_sortingAssignB

---
+++ main.js
@@ -0,0 +1,9 @@
+// import array from "./module.js";
+
+// console.log(array);
+import array from "./module.js";
+import { PI, show } from "./module.js"; 
+
+console.log(array);
+console.log(PI);
+show();
